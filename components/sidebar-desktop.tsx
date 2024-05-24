cc81f3a0fd23a9948647665b259ec44c796ff2af
gpcit <it@greenstonepackaging.com.ph>
2024-05-24 15:57:15 +0800

    fixing url

--- components/sidebar-desktop.tsx
+++ components/sidebar-desktop.tsx
@@ -35,7 +35,7 @@ export function SidebarDesktop(props: SidebarDesktopProps) {
       setTimeout(() => {
       toast.success('Logged Out Successfully', { id: toastLogout });
     }, 300);
-    await signOut({callbackUrl: 'http://10.20.10.108:3000/login', redirect:true});
+    await signOut({callbackUrl: 'http://10.20.10.108:3000', redirect:true});
     } catch (error: any) {
       console.error('Sign Out Error: ', error.message)
     }
