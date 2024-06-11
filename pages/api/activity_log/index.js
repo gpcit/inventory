5a48792046ebe42b625f366cd8d9a5f5db0cacf9
gpcit <it@greenstonepackaging.com.ph>
2024-06-11 09:12:59 +0800

    finished

--- pages/api/activity_log/index.js
+++ pages/api/activity_log/index.js
@@ -3,19 +3,27 @@ import { query } from '@/lib/db';
 export default async function handler (req, res) {
     const tableName = req.query.tableName;
     const page = req.query.page || 1
+    const itemPerPage = 3;
 
     if(req.method === 'GET') {
         try {
             const pageTotal = `SELECT COUNT(*) as total from activity_log WHERE db_table = ?`
             const userQuery = `
             SELECT * FROM activity_log
-            ORDER BY date_created desc`
-            const activityLog = await query(userQuery)
+            ORDER BY date_created desc 
+            LIMIT ? 
+            OFFSET ?`
+            const values = [itemPerPage, (page - 1 ) * itemPerPage]
 
-            // const totalCount = totalCountRows[0].total;
-            // const totalPages = Math.ceil(totalCount / itemPerPage)
+            const [activityLog, totalCountRows] = await Promise.all([
+                query(userQuery, values),
+                query(pageTotal, [tableName])
+            ]);
 
-            res.status(200).json({ activityLog })
+            const totalCount = totalCountRows[0].total;
+            const totalPages = Math.ceil(totalCount / itemPerPage)
+
+            res.status(200).json({ results: activityLog,  totalPages})
            
         } catch (error) {
             console.error('Error fetching activity log:', error);
@@ -26,5 +34,3 @@ export default async function handler (req, res) {
         res.status(405).json({ error: 'Method not allowed' });
     }
 }
-
-
